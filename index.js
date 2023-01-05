7722eaa451a9c7dcf9b3b2330fe9e9c3d7b86ee6
AhmedA <alkhatib_ahmed1995@hotmail.com>
2023-01-04 19:44:09 -0500

    added js logic for input field and button, added media queries for smaller screen size

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-notifyButton = document.querySelector(".notify-button");
+function checkEmail() {
 
-notifyButton.addEventListener("click", ()=> {
-    console.log("This button has been clicked!");
-});
+    var email = document.getElementById("emailField").value;
+
+    if (email === "") {
+        document.getElementById("message").innerHTML = "Whoops! It looks like you forgot to add your email.";
+    } else if (email.indexOf("@") === -1 || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        document.getElementById("message").innerHTML = "Please provide a valid email address.";
+    } else {
+        document.getElementById("message").innerHTML = "Thank you for subscribing!";
+    }
+};
